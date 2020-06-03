bdbedd93f74e3c922ea466e9bb4a66892f8bd497
George Rymarenko <george.rymarenko@icloud.com>
2020-06-03 13:37:22 +0200

    add brain-even game

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -6,10 +6,22 @@ import prompt
 
 def welcome_user():
     """
-    Promt user name and print greeting.
+    Promt user name.
+
+    Returns tuple consisting of greeting itself and name of the user
+
+    Returns:
+        (str, str)
+    """
+    name = prompt.string('May I have your name? ')
+    return ('Hello, {name}!'.format(name=name), name)
+
+
+def ask_for_answer():
+    """
+    Ask user to provider answer.
 
     Returns:
         str
     """
-    name = prompt.string('May I have your name? ')
-    return 'Hello, {name}!'.format(name=name)
+    return prompt.string('Your answer: ')
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,13 +1,25 @@
 """Main module."""
 
-
 from brain_games.cli import welcome_user
+
+INTRO = 'Welcome to the Brain Games!'
+
+
+def get_intro():
+    """Return intro.
+
+    Returns:
+        str
+    """
+    return INTRO
 
 
 def main():
-    """Greet user."""
-    print('Welcome to the Brain Games!\n')
-    print(welcome_user())
+    """Print intro and welcome user."""
+    print('{intro}\n'.format(intro=get_intro()))
+
+    greeting = welcome_user()[0]
+    print(greeting)
 
 
 if __name__ == '__main__':
